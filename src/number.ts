6adfdae571559746e6d6edc07ee7d52b2f8d1435
Zhangyuan Nie <yuan@znie.org>
2021-01-05 16:41:55 -0500

    fix(typing): ConfirmOpts and NumberOpts

--- src/number.ts
+++ src/number.ts
@@ -3,6 +3,7 @@ import type { PromptOpts } from './core/prompt.ts';
 import type { Result } from './core/result.ts';
 
 export interface NumberOpts extends PromptOpts {
+  type: 'number';
   min?: number;
   max?: number;
 }
